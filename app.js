329d781c55695537cd53d4f2bb89aecc91fdd136
Theo101 <ted.a.lesser@gmail.com>
2024-07-18 18:53:15 -0400

    Update package.json

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const { PORT = 3001} = process.env; 
 
-
+app.listen (PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+})
 
 
